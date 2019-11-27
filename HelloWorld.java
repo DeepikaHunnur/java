d577e482612cea577f99332cb80eac59a3d3b518
Deepika <deepika.hunnur@gmail.com>
2019-11-27 19:12:04 +0530

    Hellp

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to all women leaders of DEVOP'S team ");
+      System.out.println("Hello all, Welcome to all Girl leaders of DEVOP'S team ");
    }
 }
